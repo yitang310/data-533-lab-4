4aa5f0bb8f6635004408369d8fe187e720d19c88
chenxxiyang <35125450+chenxxiyang@users.noreply.github.com>
2019-12-09 15:45:18 -0800

    change .py

--- TestPrice.py
+++ TestPrice.py
@@ -24,11 +24,11 @@ class TestPrice(unittest.TestCase): # test class
         self.assertEqual(pr.tax_price(1,'MB'), 1.13)
 
     def test_price2(self): # test case 
-        self.assertNotEqual(pr.tax_price(1,'BC'), 1.17) 
-        self.assertNotEqual(pr.tax_price(1,'AB'), 1.08) 
-        self.assertNotEqual(pr.tax_price(1,'ON'), 1.12)
-        self.assertNotEqual(pr.tax_price(1,'NB'), 1.14)
-        self.assertNotEqual(pr.tax_price(1,'MB'), 1.15)
+        self.assertNotEqual(pr.tax_price(1,'NS'), 1.17) 
+        self.assertNotEqual(pr.tax_price(1,'NT'), 1.08) 
+        self.assertNotEqual(pr.tax_price(1,'NU'), 1.12)
+        self.assertNotEqual(pr.tax_price(1,'PE'), 1.14)
+        self.assertNotEqual(pr.tax_price(1,'QC'), 1.15)
         
     def tearDown(self):
         print("tearDown")
