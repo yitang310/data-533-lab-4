ac9ab91b46834383adc7bf2e2017956edd68bc12
chenxxiyang <35125450+chenxxiyang@users.noreply.github.com>
2019-12-09 16:29:59 -0800

    change proprty.py

--- product/information/property.py
+++ product/information/property.py
@@ -1,4 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+class TimeError(Exception):
+    pass
+
 def property(ig, ed, md):
-    print("main ingredients: {}".format(ig))
-    print("expiration date: {}".format(ed))
-    print("date of manufacture: {}".format(md))
+    try:
+        if ed < md:
+            raise TimeError()
+
+        if type(ig) is not str:
+            raise TypeError()
+
+        print("main ingredients: {}".format(ig))
+        print("expiration date: {}".format(ed))
+        print("date of manufacture: {}".format(md))
+
+    except TypeError:
+         print("Error: TypeError")
+
+    except TimeError:
+        print("Error: expiration date < date of manufacture.")
